f83d74198ab00bbf542b472740e8bda96fb9ceb2
Karbust <charparodar@gmail.com>
2020-12-18 11:33:07 +0000

    Sprint 2
    Upload files to storage

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Client extends UnicastRemoteObject {
+    public Client() throws RemoteException {
+
+    }
+}
